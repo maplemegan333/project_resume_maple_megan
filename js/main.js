c23e7ba61c64c72ec6b7e61b467d00cfeed99b40
maplemegan333 <maplemegan@ufl.edu>
2018-04-17 22:58:21 -0400

    Add tooltip

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $.backstretch([
-  "img/bg1.jpg",
-  "img/bg2.png",
-  "img/bg3.jpg"
+    'img/bg1.jpg',
+    'img/bg2.png',
+    'img/bg3.jpg'
 ], {
     fade: 750,
     duration: 4000
+});
+
+$(document).ready(function () {
+    $('.portrait').tooltipster({
+        theme: 'tooltipster-shadow',
+        side: 'bottom'
+    });
 });
